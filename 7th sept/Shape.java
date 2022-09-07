2113e30db9bdf5165da81100ea662eefc96f0cfc
Sankalpa Das <d2sankalpa@gmail.com>
2022-09-07 23:10:42 +0530

    Add files via upload

---
+++ 7th sept/Shape.java
@@ -0,0 +1,7 @@
+package com.innovation.geometry;
+import com.innovation.shapes.Circle;
+public class Shape {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Circle Circle1 = new Circle();
+	}}
