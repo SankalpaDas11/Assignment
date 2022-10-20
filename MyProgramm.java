af06342294561f464045c4f405c26f375c7cbaae
Sankalpa Das <d2sankalpa@gmail.com>
2022-10-20 19:44:22 +0530

    Update MyProgramm.java

--- MyProgramm.java
+++ MyProgramm.java
@@ -1,9 +1,9 @@
-public class MyProgramm {
+public class MyProgramm {					//class with main method
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {             
 		// TODO Auto-generated method stub
-		 DisplayMessage obj = new DisplayMessage();
-			    obj.printMessage();
+		 DisplayMessage obj = new DisplayMessage();	//creating object of DisplayMessage class
+			    obj.printMessage();			//calling print message method
 			  }}
 class DisplayMessage {
 	void printMessage() {
